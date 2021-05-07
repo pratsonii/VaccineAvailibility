602aa6734cc74495bdf99c2f4d1f63d5e9145f6f
pratiksoni <pratiksonii@yahoo.in>
2021-05-08 01:35:49 +0530

    Notification sound

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { switchMap } from 'rxjs/operators';
 import { AppServiceService } from './app-service.service';
 import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-root',
@@ -17,13 +18,21 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const sound = new Howl({
+      src: ['../assets/mp3/sound.mp3']
+    });
+
     this.service.subscribeTodata().subscribe(data => data.subscribe((data: any[]) => {
       this.listOfData = [];
       this.listOfData = data;
       this.counter = 30;
+
+      if (this.listOfData && this.listOfData.length > 0) {
+        sound.play();
+      }
     }));
 
-    const fixedValue = 30;
+    const fixedValue = 29;
     this.service.subscribeTodata().pipe(switchMap(() => interval(1000))).subscribe(val => this.counter = fixedValue - val);
   }
 }
